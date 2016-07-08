34e07c63472a8de5f9f14fffa24af8f31495511e
Caleb Mazalevskis <maikuolan@gmail.com>
2016-07-09 01:42:08 +0800

    v0.3.1
    Preparing tagged release.

--- loader.php
+++ loader.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: The loader (last modified: 2016.06.18).
+ * This file: The loader (last modified: 2016.07.09).
  */
 
 /**
@@ -23,7 +23,7 @@ if (!defined('CIDRAM')) {
     $CIDRAM = array();
 
     /** CIDRAM version number (SemVer). */
-    $CIDRAM['ScriptVersion'] = '0.3.1-DEV';
+    $CIDRAM['ScriptVersion'] = '0.3.1';
 
     /** CIDRAM version identifier (complete notation). */
     $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

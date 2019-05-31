9c3d388a53721d60de64d827a799ff86c029af44
Caleb Mazalevskis <maikuolan@gmail.com>
2019-05-31 23:41:36 +0800

    Implements channel switcher.
    Changelog excerpt:
    
    - [2019.05.31; NEW FEATURE; Maikuolan]: Added the ability to automatically
      switch between different channels when requesting a remote resource
      (e.g., when the front-end updates page attempts to update something, or
      attempts to retrieve the latest available component metadata), for
      whenever such a request fails (e.g., due to server errors, the requested
      resource no longer existing at a particular location, etc), and added a
      new configuration directive, 'disabled_channels', to optionally prevent
      CIDRAM from ever requesting resources from particular, specific channels.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2019.04.07).
+ * This file: Configuration handler (last modified: 2019.05.31).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '1.12.0';
+$CIDRAM['ScriptVersion'] = '1.13.0';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

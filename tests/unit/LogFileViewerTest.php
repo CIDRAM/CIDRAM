3932520bde286d7a60c5ae0c4f06dceb963e2ed2
naveen <kmnaveen101@gmail.com>
2019-07-20 15:31:02 +0530

    log file viewer unit test added

---
+++ tests/unit/LogFileViewerTest.php
@@ -0,0 +1,20 @@
+<?php 
+class LogFileViewerTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function testWhenNoLogFilePresentShouldReturnReason() {
+
+    }
+}

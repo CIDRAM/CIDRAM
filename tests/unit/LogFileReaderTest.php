9c6d0f380d53762bd55f1d1416cf668558d29d6f
naveen <kmnaveen101@gmail.com>
2019-07-20 15:44:10 +0530

    log file reader test created

---
+++ tests/unit/LogFileReaderTest.php
@@ -0,0 +1,22 @@
+<?php 
+class LogFileReaderTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}

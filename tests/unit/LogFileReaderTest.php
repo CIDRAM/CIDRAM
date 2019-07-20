8d91b5c92205bad4fa1408ef3ed3445bdd255516
naveen <kmnaveen101@gmail.com>
2019-07-20 15:52:30 +0530

    log file reader - initialisation passed

--- tests/unit/LogFileReaderTest.php
+++ tests/unit/LogFileReaderTest.php
@@ -1,17 +1,22 @@
 <?php 
+
+require_once __DIR__.'/../../vault/classes/LogFileReader.php';
+
 class LogFileReaderTest extends \Codeception\Test\Unit
 {
     /**
      * @var \UnitTester
      */
-    protected $tester;
+    protected $logFileReader;
     
     protected function _before()
     {
+        $this->logFileReader = new LogFileReader();
     }
 
     protected function _after()
     {
+        unset($this->logFileReader);
     }
 
     // tests

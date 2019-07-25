bae87d953a5fcbd4e84cad57191fb74ccd86118f
Caleb Mazalevskis <maikuolan@gmail.com>
2019-07-26 00:34:28 +0800

    Bump version.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2019.07.24).
+ * This file: Configuration handler (last modified: 2019.07.25).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '1.13.0';
+$CIDRAM['ScriptVersion'] = '1.13.1';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

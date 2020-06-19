74b02d1450f79e67cf45f7bf50868fa2f3c9685c
Caleb Mazalevskis <maikuolan@gmail.com>
2020-06-20 01:11:06 +0800

    Bug-fix.
    Changelog excerpt:
    - Logging broken due to missing parameter in the preg_split call in the
      recently introduced BuildPath closure; Fixed.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2020.05.01).
+ * This file: Configuration handler (last modified: 2020.06.19).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '2.4.1';
+$CIDRAM['ScriptVersion'] = '2.4.2';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

325327438e861c6be5b254ead37498e6018eadaa
Caleb Mazalevskis <maikuolan@gmail.com>
2020-07-12 19:52:26 +0800

    Refactoring + aesthetic patch.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end functions file (last modified: 2020.07.05).
+ * This file: Front-end functions file (last modified: 2020.07.12).
  */
 
 /**
@@ -3117,8 +3117,10 @@ $CIDRAM['SendEmail'] = function (array $Recipients = [], $Subject = '', $Body = 
             $Mail->AltBody = $AltBody;
 
             /** Process attachments. */
-            foreach ($Attachments as $Attachment) {
-                $Mail->addAttachment($Attachment);
+            if (is_array($Attachments)) {
+                foreach ($Attachments as $Attachment) {
+                    $Mail->addAttachment($Attachment);
+                }
             }
 
             /** Send it! */

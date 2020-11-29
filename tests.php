14329b2750b70a1dd68334693c17024647ea927a
Caleb Mazalevskis <maikuolan@gmail.com>
2020-11-29 15:36:20 +0800

    Improve tests.

--- tests.php
+++ tests.php
@@ -15,6 +15,7 @@ if (!isset($_SERVER['COMPOSER_BINARY'])) {
 
 // Suppress unexpected errors from output and exit early as a failure when encountered.
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    echo 'Error triggered: ' . $errstr . PHP_EOL;
     exit(1);
 });
 
@@ -32,6 +33,7 @@ $CIDRAM_Config['supplementary_cache_options']['enable_apcu'] = true;
 // Load each required file or exit immediately if any of them don't exist.
 foreach (['functions.php', 'config.php', 'lang.php', 'frontend_functions.php'] as $File) {
     if (!is_readable($CIDRAM['Vault'] . $File)) {
+        echo $File . '.php is not readable.' . PHP_EOL;
         exit(2);
     }
     require $CIDRAM['Vault'] . $File;
@@ -39,6 +41,7 @@ foreach (['functions.php', 'config.php', 'lang.php', 'frontend_functions.php'] a
 
 // Exit immediately if the loader of the configuration file doesn't exist.
 if (!is_readable(__DIR__ . DIRECTORY_SEPARATOR . 'loader.php') || !is_readable($CIDRAM['Vault'] . 'config.ini.RenameMe')) {
+    echo 'Loader or configuration is not readable.' . PHP_EOL;
     exit(3);
 }
 
@@ -78,6 +81,7 @@ $Expected = [
     '127.0.0.1/32'
 ];
 if ($Expected !== $CIDRAM['ExpandIPv4']('127.0.0.1')) {
+    echo 'ExpandIPv4 failed.' . PHP_EOL;
     exit(4);
 }
 
@@ -213,10 +217,11 @@ $Expected = [
     '0::1/128'
 ];
 if ($Expected !== $CIDRAM['ExpandIPv6']('::1')) {
+    echo 'ExpandIPv6 failed.' . PHP_EOL;
     exit(5);
 }
 
 restore_error_handler();
 
-// All tests passed.
+echo 'All tests passed.' . PHP_EOL;
 exit(0);

b45443a911aaeaa5c845ab6aa12abe3fe632ca75
Caleb Mazalevskis <maikuolan@gmail.com>
2021-03-21 13:43:38 +0800

    Fix PHP CS Fixer complaint.

--- vault/functions.php
+++ vault/functions.php
@@ -2477,7 +2477,8 @@ $CIDRAM['IsSensitive'] = function (string $URI): bool {
         ')|signup)|act(ion)?=(edit|reg)|(activate|confirm|login|newuser|reg(ist(' .
         'er|ration))?|sign(in|up))(\.php|=)|special:userlogin&|verifyemail|wp-co' .
         'mments-post~',
-    $URI);
+        $URI
+    );
 };
 
 /** Make sure the vault is defined so that tests don't break. */

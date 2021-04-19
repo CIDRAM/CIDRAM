4050597c823b0e535afc01a43d7647d30f894df7
Caleb Mazalevskis <maikuolan@gmail.com>
2021-04-19 21:57:31 +0800

    Bug-fix (#214).
    Changelog excerpt:
    - BuildPath could potentially trigger warnings when open_basedir is
      defined, causing logging, among various other internal file operations,
      to fail (related to PHP bug 69240); Fixed.

--- vault/functions.php
+++ vault/functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Functions file (last modified: 2021.04.04).
+ * This file: Functions file (last modified: 2021.04.19).
  */
 
 /** Autoloader for CIDRAM classes. */
@@ -1634,13 +1634,16 @@ $CIDRAM['ResetBypassFlags'] = function () use (&$CIDRAM): void {
  *      returns an empty string.
  */
 $CIDRAM['BuildPath'] = function (string $Path, bool $PointsToFile = true) use (&$CIDRAM): string {
-    /** Guard. */
-    if (!$Path) {
+    /** Input guard. */
+    if (!strlen($Path)) {
         return '';
     }
 
     /** Applies time/date replacements. */
     $Path = $CIDRAM['TimeFormat']($CIDRAM['Now'], $Path);
+
+    /** We'll skip is_dir/mkdir calls if open_basedir is populated (to avoid PHP bug #69240). */
+    $Restrictions = strlen(ini_get('open_basedir')) > 0;
 
     /** Split path into steps. */
     $Steps = preg_split('~[\\\/]~', $Path, -1, PREG_SPLIT_NO_EMPTY);
@@ -1658,9 +1661,14 @@ $CIDRAM['BuildPath'] = function (string $Path, bool $PointsToFile = true) use (&
         if (preg_match('~^\.+$~', $Step)) {
             continue;
         }
-        if (!is_dir($Rebuilt) && !mkdir($Rebuilt)) {
+        if (!$Restrictions && !is_dir($Rebuilt) && !mkdir($Rebuilt)) {
             return '';
         }
+    }
+
+    /** Ensure rebuilt is defined. */
+    if (!isset($Rebuilt)) {
+        $Rebuilt = '';
     }
 
     /** Return an empty string if the final rebuilt path isn't writable. */

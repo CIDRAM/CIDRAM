1485951a219d66e8750f451140c0d0733eb9161f
Caleb Mazalevskis <maikuolan@gmail.com>
2022-03-13 22:28:30 +0800

    Aesthetic patch.
    Warnings didn't look too great when scaling down the browser width, thus
    pushing warnings to cover multiple lines. Commit changes warnings to
    inline-block to improve the aesthetic.

--- vault/frontend.php
+++ vault/frontend.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end handler (last modified: 2022.03.11).
+ * This file: Front-end handler (last modified: 2022.03.13).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -189,10 +189,10 @@ if (empty($CIDRAM['Config']['signatures']['ipv4']) && empty($CIDRAM['Config']['s
 }
 
 /** Prepare warnings. */
-$CIDRAM['FE']['Warnings'] = count($CIDRAM['Warnings']) ? "\n<div class=\"center\"><span class=\"warning\">" . implode(
-    "</span><br />\n<span class=\"warning\">",
+$CIDRAM['FE']['Warnings'] = count($CIDRAM['Warnings']) ? "\n<div class=\"center\"><div class=\"warning\">" . implode(
+    "</div>\n<div class=\"warning\">",
     $CIDRAM['Warnings']
-) . '</span></div><hr />' : '';
+) . '</div></div><hr />' : '';
 
 /** Cleanup. */
 unset($CIDRAM['Warnings']);

07f1e989d6fa5a1a0ff43307e605b1bb1bbc58f9
Caleb Mazalevskis <maikuolan@gmail.com>
2022-06-17 15:18:15 +0800

    Slightly improve RTL support.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end functions file (last modified: 2022.06.16).
+ * This file: Front-end functions file (last modified: 2022.06.17).
  */
 
 /**
@@ -3985,7 +3985,9 @@ $CIDRAM['ArrayToClickableList'] = function (array $Arr = [], string $DeleteKey =
             }
             $Class = ($Key === $CIDRAM['L10N']->getString('field_size') || $Key === $CIDRAM['L10N']->getString('label_expires')) ? 'txtRd' : 's';
             $Text = ($Count === 1 && $Key === 0) ? $Value : $Key . ($Class === 's' ? ' => ' : ' ') . $Value;
-            $Output .= '<code class="' . $Class . '" style="word-wrap:break-word;word-break:break-all">' . str_replace(['<', '>'], ['&lt;', '&gt;'], $Text) . '</code>' . $Delete;
+            $Output .= '<code class="' . $Class . '" style="word-wrap:break-word;word-break:break-all">' . $CIDRAM['LTRinRTF'](
+                str_replace(['<', '>'], ['&lt;', '&gt;'], $Text)
+            ) . '</code>' . $Delete;
         }
         $Output .= '</li>' . ($Depth === 0 ? '<br /></span>' : '');
     }
@@ -4062,8 +4064,8 @@ $CIDRAM['LTRinRTF'] = function (string $String = '') use (&$CIDRAM): string {
     /** Modify the string to better suit RTL directionality and return it. */
     while (true) {
         $NewString = preg_replace(
-            ['~^(.+)-&gt;(.+)$~i', '~^(.+)➡(.+)$~i'],
-            ['\2&lt;-\1', '\2⬅\1'],
+            ['~^(.+)( +)-&gt;( +)(.+)$~i', '~^(.+)-&gt;(.+)$~i', '~^(.+)( +)➡( +)(.+)$~i', '~^(.+)➡(.+)$~i', '~^(.+)( +)=&gt;( +)(.+)$~i', '~^(.+)=&gt;(.+)$~i'],
+            ['\4\2&lt;-\3\1', '\2&lt;-\1', '\4\2⬅\3\1', '\2⬅\1', '\4\2&lt;=\3\1', '\2&lt;=\1'],
             $String
         );
         if ($NewString === $String) {

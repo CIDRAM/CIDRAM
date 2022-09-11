4d029b0da0d4d8ca53924fba1364ba2c57f019dc
Caleb Mazalevskis <maikuolan@gmail.com>
2022-09-11 12:10:48 +0800

    Sensitive page detection patch (#337).

--- vault/CIDRAM/CIDRAM/Core.php
+++ vault/CIDRAM/CIDRAM/Core.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: The CIDRAM core (last modified: 2022.07.24).
+ * This file: The CIDRAM core (last modified: 2022.09.11).
  */
 
 namespace CIDRAM\CIDRAM;
@@ -2723,10 +2723,10 @@ class Core
     public function isSensitive(string $URI): bool
     {
         return preg_match(
-            '~/(comprofiler|user)/(login|register)|=(activate|login|regist(er|ration' .
-            ')|signup)|act(ion)?=(edit|reg)|(activate|confirm|login|newuser|reg(ist(' .
-            'er|ration))?|sign(in|up))(\.php|=)|special:userlogin&|verifyemail|wp-co' .
-            'mments-post~',
+            '~/(?:comprofiler|user)/(?:contact|login|register)|=(?:activate|contact|' .
+            'login|regist(?:er|ration)|signup)|act(?:ion)?=(?:edit|reg)|(?:activate|' .
+            'con(?:firm|tact)|login|newuser|reg(?:ist(?:er|ration))?|signin|signup)(' .
+            '?:\.php|=)|special:userlogin&|verifyemail|wp-comments-post~i',
             $URI
         );
     }

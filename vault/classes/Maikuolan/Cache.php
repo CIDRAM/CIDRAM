92b6c2e9577a69f51031ba7ee7c381bfd452369c
Caleb Mazalevskis <maikuolan@gmail.com>
2023-02-23 23:30:38 +0800

    Common classes update.

--- vault/classes/Maikuolan/Cache.php
+++ vault/classes/Maikuolan/Cache.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * A simple, unified cache handler (last modified: 2023.02.21).
+ * A simple, unified cache handler (last modified: 2023.02.23).
  *
  * This file is a part of the "common classes package", utilised by a number of
  * packages and projects, including CIDRAM and phpMussel.
@@ -180,7 +180,7 @@ class Cache
      *      be needed by some implementations to ensure compatibility).
      * @link https://github.com/Maikuolan/Common/tags
      */
-    const VERSION = '1.9.4';
+    const VERSION = '1.9.5';
 
     /**
      * Construct object and set working data if needed.

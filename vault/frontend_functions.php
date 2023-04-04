b386bc4ff2128e6e764d6f24f8edb867f6b91e35
Caleb Mazalevskis <maikuolan@gmail.com>
2023-04-04 19:04:17 +0800

    Aesthetic patch.
    Make really long log entries horizontally scrollable.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end functions file (last modified: 2023.03.23).
+ * This file: Front-end functions file (last modified: 2023.04.04).
  */
 
 /**
@@ -1181,7 +1181,7 @@ $CIDRAM['Formatter'] = function (&$In, $BlockLink = '', $Current = '', $FieldSep
     $BlockEnd = 0;
     while ($BlockEnd !== false) {
         $Darken = empty($Darken);
-        $Style = '<div class="h' . ($Darken ? 'B' : 'W') . ' hFd fW">';
+        $Style = '<div style="overflow:visible auto" class="h' . ($Darken ? 'B' : 'W') . ' hFd fW">';
         $BlockEnd = strpos($Out, $BlockSeparator, $BlockStart);
         $In[] = $Style . substr($Out, $BlockStart, $BlockEnd - $BlockStart + $BlockSeparatorLen) . '</div>';
         $BlockStart = $BlockEnd + $BlockSeparatorLen;

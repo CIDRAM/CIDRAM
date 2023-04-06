f13ee75eb41f51788784d2f2cc8e35187955bcaa
Caleb Mazalevskis <maikuolan@gmail.com>
2023-04-06 19:18:02 +0800

    Bug-fix.
    Changelog excerpt:
    - Exit code 1 responses generated via run parameters in signatures weren't
      being honoured (although nothing in CIDRAM currently actually returns
      exit code 1 anyway, so this doesn't have any practical bearing); Fixed.

--- vault/functions.php
+++ vault/functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Functions file (last modified: 2023.03.18).
+ * This file: Functions file (last modified: 2023.04.06).
  */
 
 /**
@@ -452,7 +452,7 @@ $CIDRAM['CheckFactors'] = function (array $Files, array $Factors) use (&$CIDRAM)
                     $CIDRAM['ZeroOutBlockInfo']();
                     break 2;
                 }
-                if ($Category === 'Deny' && $Signature !== '') {
+                if (($Category === 'Deny' || $RunExitCode === 1) && $Signature !== '') {
                     $DenyMatched = false;
                     if (!$CIDRAM['CIDRAM_sapi']) {
                         foreach ([

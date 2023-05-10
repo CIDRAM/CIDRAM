CIDRAM/CIDRAM

commit 6ac44f28160ff2c654bead1ed5622960faa26431

IP test page and block event simulation patch (#464).
Changelog excerpt:
- Results at the IP test page now include information about redirects via
  silent mode and auxiliary rules.

vault/CIDRAM/CIDRAM/Protect.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Protect traits (last modified: 2023.05.07).
+ * This file: Protect traits (last modified: 2023.05.08).
  */
 
 namespace CIDRAM\CIDRAM;
@@ -769,7 +769,7 @@ trait Protect
                 /** Append default L10N data. */
                 $this->CIDRAM['Parsables'] += $this->L10N->Data;
 
-                if (!$this->Configuration['general']['silent_mode']) {
+                if ($this->Configuration['general']['silent_mode'] !== '') {
                     /** Enforce output template suppression. */
                     if (
                         (!empty($this->CIDRAM['Banned']) && isset($this->Shorthand['Banned:Suppress'])) ||

vault/installed.yml
@@ -40,7 +40,7 @@ CIDRAM Core:
    Checksum: "d27710669427e8f88fb849161517d7c7afc4635c29688141ca90af490b84b85d:15275"
   CIDRAM/CIDRAM/Protect.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/Protect.php"
-   Checksum: "492c40c030fd283f2a34ac49daa58866b4165a944ff8e59cd6f1b22267b24370:56417"
+   Checksum: "8a8695c1148f793edb1c7c4a5193239cef13d76258de890f1c8d142d1b88358e:56423"
   CIDRAM/CIDRAM/ReCaptcha.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/ReCaptcha.php"
    Checksum: "e4aa4bdddb2d087ee09c83a9c5a48845a8f3d0b12b222bfd5c9f584b51c8913c:14733"
@@ -388,7 +388,7 @@ CIDRAM Front-End:
    Checksum: "78f5383904a34ba37d5d0d95dc0ba9ddefe640b692ba9652537cec20d63e9280:4547"
   CIDRAM/CIDRAM/FrontEnd.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/FrontEnd.php"
-   Checksum: "5a27bb13cbfc01442f7c718fcbd082f710974324c57cf3f81d8cc4a8699ffdad:323490"
+   Checksum: "c26810b6f4676cd105b67f23e73fc30dbf4028c5c67e2906c9acfcc180dd534c:323788"
   CIDRAM/CIDRAM/FrontEndMethods.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/FrontEndMethods.php"
    Checksum: "1261cd2f71406ee7c64082cdf9419b345973d63ab244eda5c67820c84b70d29b:60839"
@@ -400,7 +400,7 @@ CIDRAM Front-End:
    Checksum: "8a813d7db234c11fa45944cb91ecd8b26f7b568c8927ed029bef4e77f627744a:39326"
   CIDRAM/CIDRAM/SimulateBlockEvent.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/SimulateBlockEvent.php"
-   Checksum: "14ef93a2650eb681c24653e9765e67faaef51638d1afdfffe1e2c39ca7536f44:14620"
+   Checksum: "4ccf44d60bd897b8157c08068b637c0bd45b2bb9a642546e221845124fb02bb0:14627"
   CIDRAM/CIDRAM/Updater.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/Updater.php"
    Checksum: "0871c8c49c5fe869410290802daddc7b02a41412757dd2dd5684d047767b3c71:66523"

vault/CIDRAM/CIDRAM/FrontEnd.php
@@ -4040,10 +4040,10 @@ class FrontEnd extends Core
                             );
                         }
                     } elseif ($this->BlockInfo['Ignored']) {
-                        $this->CIDRAM['ThisIP']['YesNo'] = $this->L10N->getString('field_blocked') . $this->L10N->getString('pair_separator') . $this->L10N->getString('response_no') . '(' . $this->L10N->getString('state_ignored') . ')' . $this->BlockInfo['Ignored'];
+                        $this->CIDRAM['ThisIP']['YesNo'] = $this->L10N->getString('field_blocked') . $this->L10N->getString('pair_separator') . $this->L10N->getString('response_no') . ' (' . $this->L10N->getString('state_ignored') . ') ' . $this->BlockInfo['Ignored'];
                         $this->CIDRAM['ThisIP']['StatClass'] = 'txtOe';
                     } elseif (!empty($this->CIDRAM['Aux Redirect']) && !empty($this->CIDRAM['Aux Status Code'])) {
-                        $this->CIDRAM['ThisIP']['YesNo'] = $this->L10N->getString('field_blocked') . $this->L10N->getString('pair_separator') . $this->L10N->getString('response_no') . '(' . $this->L10N->getString('response_redirected') . ')';
+                        $this->CIDRAM['ThisIP']['YesNo'] = $this->L10N->getString('field_blocked') . $this->L10N->getString('pair_separator') . $this->L10N->getString('response_no') . ' (' . $this->L10N->getString('response_redirected') . ')';
                         $this->CIDRAM['ThisIP']['StatClass'] = 'txtOe';
                     } else {
                         $this->CIDRAM['ThisIP']['YesNo'] = $this->L10N->getString('field_blocked') . $this->L10N->getString('pair_separator') . $this->L10N->getString('response_no');
@@ -4053,12 +4053,13 @@ class FrontEnd extends Core
                         if ($this->CIDRAM['ThisIP']['StatClass'] === 'txtGn') {
                             $this->CIDRAM['ThisIP']['StatClass'] = 'txtOe';
                         }
-                        $this->CIDRAM['ThisIP']['YesNo'] .= '<br />' . $this->ltrInRtf(sprintf(
-                            '%1$s <%2$d> ➡ %3$s',
-                            $this->L10N->getString('response_redirected'),
-                            $this->CIDRAM['Aux Status Code'],
-                            '<code>' . $this->CIDRAM['Aux Redirect'] . '</code>'
-                        ));
+                        $this->CIDRAM['ThisIP']['YesNo'] .= '<br />' . $this->ltrInRtf(
+                            $this->L10N->getString('response_redirected') . ' <' . $this->CIDRAM['Aux Status Code'] . '> ➡ <code>' . $this->CIDRAM['Aux Redirect'] . '</code>'
+                        );
+                    } elseif ($this->BlockInfo['SignatureCount'] && $this->Configuration['general']['silent_mode'] !== '') {
+                        $this->CIDRAM['ThisIP']['YesNo'] .= '<br />' . $this->ltrInRtf(
+                            $this->L10N->getString('response_redirected') . ' <301> ➡ <code>' . $this->Configuration['general']['silent_mode'] . '</code>'
+                        );
                     }
                     $this->CIDRAM['ThisIP']['YesNo'] .= '<br />' . $this->L10N->getString('field_tracking') . $this->L10N->getString('pair_separator');
                     if (isset($this->CIDRAM['Trackable'])) {

vault/CIDRAM/CIDRAM/SimulateBlockEvent.php
@@ -251,7 +251,7 @@ trait SimulateBlockEvent
          */
         if ($this->BlockInfo['SignatureCount'] > 0) {
             $this->CIDRAM['ThisStatusHTTP'] = (
-                ($this->Configuration['general']['silent_mode'] && ($Try = 301)) ||
+                ($this->Configuration['general']['silent_mode'] !== '' && ($Try = 301)) ||
                 (!empty($this->CIDRAM['Banned']) && $this->Configuration['general']['ban_override'] > 400 && ($Try = $this->Configuration['general']['ban_override'])) ||
                 (!empty($this->CIDRAM['RL_Status']) && $this->BlockInfo['SignatureCount'] === 1 && ($Try = 429)) ||
                 (!empty($this->CIDRAM['Aux Status Code']) && $this->CIDRAM['Aux Status Code'] > 400 && ($Try = $this->CIDRAM['Aux Status Code'])) ||

commit 6f2aadb23dd9a75ac6738c70b7d782a18e268f35

Bug-fix (#466).
Fixes bug introduced by 6ac44f28160ff2c654bead1ed5622960faa26431.

vault/CIDRAM/CIDRAM/Protect.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Protect traits (last modified: 2023.05.08).
+ * This file: Protect traits (last modified: 2023.05.10).
  */
 
 namespace CIDRAM\CIDRAM;
@@ -769,7 +769,7 @@ trait Protect
                 /** Append default L10N data. */
                 $this->CIDRAM['Parsables'] += $this->L10N->Data;
 
-                if ($this->Configuration['general']['silent_mode'] !== '') {
+                if ($this->Configuration['general']['silent_mode'] === '') {
                     /** Enforce output template suppression. */
                     if (
                         (!empty($this->CIDRAM['Banned']) && isset($this->Shorthand['Banned:Suppress'])) ||

vault/installed.yml
@@ -1,6 +1,6 @@
 CIDRAM Core:
  Name: "CIDRAM Core"
- Version: "3.2.1-DEV+231260"
+ Version: "3.2.1-DEV+231290"
  Dependencies:
   PHP: "^7.2|^8"
   CIDRAM Front-End: "Latest"
@@ -40,7 +40,7 @@ CIDRAM Core:
    Checksum: "d27710669427e8f88fb849161517d7c7afc4635c29688141ca90af490b84b85d:15275"
   CIDRAM/CIDRAM/Protect.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/Protect.php"
-   Checksum: "8a8695c1148f793edb1c7c4a5193239cef13d76258de890f1c8d142d1b88358e:56423"
+   Checksum: "1c6b60ea70f32bc90474fa65e59d923481d8163c15a65724cfac24659873379b:56423"
   CIDRAM/CIDRAM/ReCaptcha.php:
    From: "https://raw.githubusercontent.com/CIDRAM/CIDRAM/v3/vault/CIDRAM/CIDRAM/ReCaptcha.php"
    Checksum: "e4aa4bdddb2d087ee09c83a9c5a48845a8f3d0b12b222bfd5c9f584b51c8913c:14733"
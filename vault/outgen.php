CIDRAM/CIDRAM

commit ef4e6e9a8109ca1581dd1217511749bcafd7c6c3

IP test page and block event simulation patch (#464).
Changelog excerpt:
- Results at the IP test page now include information about redirects via
  silent mode and auxiliary rules.

vault/components.dat
@@ -61,7 +61,7 @@ CIDRAM Core:
    - "9284fbad3c6b2549616492089703551569d07fc3e2560724ec6575f608632590:9250"
    - "597fca2e0c424d6e3303221f35fa3b68f4083cbccac9b344dc039d8f76852fbd:101285"
    - "8821191af5f524203f75ca81024cf0317c6629c3de6ac41634f665085361e319:5641"
-   - "791d38efdd56e0eb5d5a50d8ab8dac7aa4d521d51f3e2c9864210dc1027675f4:43574"
+   - "01abb5d7f870273e9c4bab6b977a9196f79d0264f906852532f8b1854ba128f6:43580"
    - "e48dc4d8b942bc26580caebdfc7ff79bfbb3807003c918c739c9e7dfdb61bf5e:1204"
    - "67dbe791d181719036b31e00056ac418e2a9633a5ab9d8b82a18369aabe1e6c6:2434"
    - "59af0e7769227c8bd8a65287ceadc2c93b0b2a314bfff805e87ddf9cb55cc4ae:7564"
@@ -225,7 +225,7 @@ CIDRAM Front-End:
    - "3bc42724fbf0651e7225d54e1761948c1d624c1d853549aa0809721abf700a07:722"
    - "828d6cb0f53f3ff440d9901f295f8c6a0eefb295998445b81436158e94b40f2e:1391"
    - "9acc8e42f33d79379bfa11e2d0c88685286f4548811d70bcb1f3151e68e18cf1:2303"
-   - "1ddae7356432059f62f271cbcf558c414d2a3f166739ddd183fa2162eb8c3078:309740"
+   - "59ab8f988e0c52fcfb11fc50e114fcb4bab5bd0a2a062194ca1c3855aba9c600:310028"
    - "2b47cbc92c6f24cf661e3b353faf1bb90ebeec4607265554997749da2e9c788c:229984"
  Reannotate: "components.dat"
 Common Classes Package:

vault/outgen.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Output generator (last modified: 2023.05.07).
+ * This file: Output generator (last modified: 2023.05.08).
  */
 
 /** Initialise cache. */
@@ -781,7 +781,7 @@ if ($CIDRAM['BlockInfo']['SignatureCount'] > 0) {
         header('Status: 429 ' . $CIDRAM['RL_Status']);
         header('Retry-After: ' . floor($CIDRAM['Config']['rate_limiting']['allowance_period'] * 3600));
         $CIDRAM['HTML'] = '';
-    } elseif (!$CIDRAM['Config']['general']['silent_mode']) {
+    } elseif ($CIDRAM['Config']['general']['silent_mode'] !== '') {
         /** Fetch appropriate status code based on either "forbid_on_block" or "Aux Status Code". */
         if ((
             !empty($CIDRAM['Aux Status Code']) &&

vault/frontend.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end handler (last modified: 2023.05.07).
+ * This file: Front-end handler (last modified: 2023.05.08).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -4195,7 +4195,7 @@ elseif ($CIDRAM['QueryVars']['cidram-page'] === 'ip-test' && $CIDRAM['FE']['Perm
                 );
                 $CIDRAM['ThisIP']['StatClass'] = 'txtOe';
             } elseif (!empty($CIDRAM['Aux Redirect']) && !empty($CIDRAM['Aux Status Code'])) {
-                $CIDRAM['ThisIP']['YesNo'] = $CIDRAM['L10N']->getString('response_no') . '(' . $CIDRAM['L10N']->getString('response_redirected') . ')';
+                $CIDRAM['ThisIP']['YesNo'] = $CIDRAM['L10N']->getString('response_no') . ' (' . $CIDRAM['L10N']->getString('response_redirected') . ')';
                 $CIDRAM['ThisIP']['StatClass'] = 'txtOe';
             } else {
                 $CIDRAM['ThisIP']['YesNo'] = $CIDRAM['L10N']->getString('response_no');
@@ -4205,12 +4205,13 @@ elseif ($CIDRAM['QueryVars']['cidram-page'] === 'ip-test' && $CIDRAM['FE']['Perm
                 if ($CIDRAM['ThisIP']['StatClass'] === 'txtGn') {
                     $CIDRAM['ThisIP']['StatClass'] = 'txtOe';
                 }
-                $CIDRAM['ThisIP']['YesNo'] .= '<br />' . $CIDRAM['LTRinRTF'](sprintf(
-                    '%1$s <%2$d> ➡ %3$s',
-                    $CIDRAM['L10N']->getString('response_redirected'),
-                    $CIDRAM['Aux Status Code'],
-                    '<code>' . $CIDRAM['Aux Redirect'] . '</code>'
-                ));
+                $CIDRAM['ThisIP']['YesNo'] .= '<br />' . $CIDRAM['LTRinRTF'](
+                    $CIDRAM['L10N']->getString('response_redirected') . ' <' . $CIDRAM['Aux Status Code'] . '> ➡ <code>' . $CIDRAM['Aux Redirect'] . '</code>'
+                );
+            } elseif ($CIDRAM['BlockInfo']['SignatureCount'] && $CIDRAM['Config']['general']['silent_mode'] !== '') {
+                $CIDRAM['ThisIP']['YesNo'] .= '<br />' . $CIDRAM['LTRinRTF'](
+                    $CIDRAM['L10N']->getString('response_redirected') . ' <301> ➡ <code>' . $CIDRAM['Config']['general']['silent_mode'] . '</code>'
+                );
             }
             if (isset($CIDRAM['Trackable'])) {
                 $CIDRAM['ThisIP']['YesNo'] .= '<br />' . $CIDRAM['L10N']->getString('field_tracking') . ' – ' . $CIDRAM['L10N']->getString((

commit e9c4c3d46d6a20634841ba1d966804d7973c9b96

Bug-fix (#466).
Fixes bug introduced by ef4e6e9a8109ca1581dd1217511749bcafd7c6c3.

vault/components.dat
@@ -1,7 +1,7 @@
 ---
 CIDRAM Core:
  Name: "CIDRAM Core"
- Version: "2.12.1-DEV+231260"
+ Version: "2.12.1-DEV+231290"
  Dependencies:
   PHP: "^7.2|^8"
   Common Classes Package: "^2.9.6"
@@ -61,7 +61,7 @@ CIDRAM Core:
    - "9284fbad3c6b2549616492089703551569d07fc3e2560724ec6575f608632590:9250"
    - "597fca2e0c424d6e3303221f35fa3b68f4083cbccac9b344dc039d8f76852fbd:101285"
    - "8821191af5f524203f75ca81024cf0317c6629c3de6ac41634f665085361e319:5641"
-   - "01abb5d7f870273e9c4bab6b977a9196f79d0264f906852532f8b1854ba128f6:43580"
+   - "db17029ac8249fd58019b3508a76ee70db58ab56368b56844cdb1c073d6bc175:43580"
    - "e48dc4d8b942bc26580caebdfc7ff79bfbb3807003c918c739c9e7dfdb61bf5e:1204"
    - "67dbe791d181719036b31e00056ac418e2a9633a5ab9d8b82a18369aabe1e6c6:2434"
    - "59af0e7769227c8bd8a65287ceadc2c93b0b2a314bfff805e87ddf9cb55cc4ae:7564"

vault/outgen.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Output generator (last modified: 2023.05.08).
+ * This file: Output generator (last modified: 2023.05.10).
  */
 
 /** Initialise cache. */
@@ -781,7 +781,7 @@ if ($CIDRAM['BlockInfo']['SignatureCount'] > 0) {
         header('Status: 429 ' . $CIDRAM['RL_Status']);
         header('Retry-After: ' . floor($CIDRAM['Config']['rate_limiting']['allowance_period'] * 3600));
         $CIDRAM['HTML'] = '';
-    } elseif ($CIDRAM['Config']['general']['silent_mode'] !== '') {
+    } elseif ($CIDRAM['Config']['general']['silent_mode'] === '') {
         /** Fetch appropriate status code based on either "forbid_on_block" or "Aux Status Code". */
         if ((
             !empty($CIDRAM['Aux Status Code']) &&
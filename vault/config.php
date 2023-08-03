d6d0b0172cb30fc524d36f6e554e2ee6ff8bcddd
Caleb Mazalevskis <maikuolan@gmail.com>
2023-08-03 16:38:55 +0800

    Fix wordwrap.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2023.06.16).
+ * This file: Configuration handler (last modified: 2023.08.02).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '2.13.0';
+$CIDRAM['ScriptVersion'] = '2.13.1';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

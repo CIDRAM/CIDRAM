f9614fd48d7a2371f70f3a786a8701634117d5ba
Caleb Mazalevskis <maikuolan@gmail.com>
2023-08-06 01:41:30 +0800

    New feature.
    Changelog excerpt:
    - Added the ability to instruct CIDRAM to detect the method for testing
      auxiliary rules conditions automatically.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2023.08.02).
+ * This file: Configuration handler (last modified: 2023.08.04).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '1.26.1';
+$CIDRAM['ScriptVersion'] = '1.27.0';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

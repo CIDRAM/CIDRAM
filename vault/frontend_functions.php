c43239c68ef211896ebd14da06a5479ae0e9f258
Caleb Mazalevskis <maikuolan@gmail.com>
2023-08-13 22:47:22 +0800

    Bug-fix.
    Auxiliary rules automatic method detection boundary check wasn't quoted
    properly; Fixed.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Front-end functions file (last modified: 2023.08.08).
+ * This file: Front-end functions file (last modified: 2023.08.13).
  */
 
 /**
@@ -3817,6 +3817,7 @@ $CIDRAM['AuxGenerateFEData'] = function (bool $Mode = false) use (&$CIDRAM): str
                             $Boundary = substr($Value, 0, 1);
                             if (
                                 !preg_match('~^[\0-\x20\dA-Za-z\xC0-\xFF]$~', $Boundary) &&
+                                ($Boundary = preg_quote($Boundary) || true) &&
                                 preg_match($Boundary === '~' ? '/^' . $Boundary . '.+' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$/' : '~^' . $Boundary . '.*' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$~', $Value)
                             ) {
                                 $Operator = '≇';
@@ -3853,6 +3854,7 @@ $CIDRAM['AuxGenerateFEData'] = function (bool $Mode = false) use (&$CIDRAM): str
                             $Boundary = substr($Value, 0, 1);
                             if (
                                 !preg_match('~^[\0-\x20\dA-Za-z\xC0-\xFF]$~', $Boundary) &&
+                                ($Boundary = preg_quote($Boundary) || true) &&
                                 preg_match($Boundary === '~' ? '/^' . $Boundary . '.+' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$/' : '~^' . $Boundary . '.*' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$~', $Value)
                             ) {
                                 $Operator = '≅';
--- vault/functions.php
+++ vault/functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Functions file (last modified: 2023.08.06).
+ * This file: Functions file (last modified: 2023.08.13).
  */
 
 /** Autoloader for CIDRAM classes. */
@@ -1910,6 +1910,7 @@ $CIDRAM['AuxMatch'] = function ($Criteria, $Actual, string $Method = '') use (&$
             $Boundary = substr($TestCase, 0, 1);
             if (
                 !preg_match('~^[\0-\x20\dA-Za-z\xC0-\xFF]$~', $Boundary) &&
+                ($Boundary = preg_quote($Boundary) || true) &&
                 preg_match($Boundary === '~' ? '/^' . $Boundary . '.+' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$/' : '~^' . $Boundary . '.*' . $Boundary . 'i?m?s?x?A?D?S?U?u?n?$~', $TestCase)
             ) {
                 $Operator = '≅';

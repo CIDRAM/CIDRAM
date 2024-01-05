382167af2de0b9f4104c183abfcec5c295baa67c
Caleb Mazalevskis <maikuolan@gmail.com>
2024-01-05 18:46:36 +0800

    Verification update.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2023.12.01).
+ * This file: Configuration handler (last modified: 2024.01.05).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '1.27.2';
+$CIDRAM['ScriptVersion'] = '1.28.0';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];

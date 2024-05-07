2d96c50e48844a9e0f6bfcb479ba0ee783d67ea8
Caleb Mazalevskis <maikuolan@gmail.com>
2024-05-07 18:12:00 +0800

    Bot user agents module update (#588).

--- vault/modules/botua.php
+++ vault/modules/botua.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Bot user agents module (last modified: 2024.05.05).
+ * This file: Bot user agents module (last modified: 2024.05.07).
  *
  * False positive risk (an approximate, rough estimate only): « [ ]Low [x]Medium [ ]High »
  */
@@ -414,10 +414,11 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
     /**
      * @link https://github.com/CIDRAM/CIDRAM/issues/493
      * @link https://github.com/CIDRAM/CIDRAM/issues/557
+     * @link https://github.com/CIDRAM/CIDRAM/issues/588
      * @link https://trunc.org/learning/the-mozlila-user-agent-bot
      */
     if (
-        $this->trigger(strpos($UANoSpace, 'mozlila') !== false || strpos($UANoSpace, 'moblie') !== false, 'Attack UA') // 2023.08.10 mod 2024.01.18
+        $this->trigger(strpos($UANoSpace, 'mozlila') !== false || strpos($UANoSpace, 'moblie') !== false || $UANoSpace === 'mozila/5.0', 'Attack UA') // 2023.08.10 mod 2024.05.07
     ) {
         $this->Reporter->report([15, 19, 20, 21], ['User agent cited by various attack tools, rootkits, backdoors, webshells, and malware detected.'], $this->BlockInfo['IPAddr']);
         $this->CIDRAM['Tracking options override'] = 'extended';

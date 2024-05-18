1f96fe39f8c626704309ebd644dc21729957e9b9
Caleb Mazalevskis <maikuolan@gmail.com>
2024-05-19 00:31:01 +0800

    Extras module update.

--- vault/modules/extras.php
+++ vault/modules/extras.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Optional security extras module (last modified: 2024.05.14).
+ * This file: Optional security extras module (last modified: 2024.05.19).
  *
  * False positive risk (an approximate, rough estimate only): « [ ]Low [x]Medium [ ]High »
  */
@@ -98,7 +98,7 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
             'test/wp-includes/wlwmanifest\.xml|' .
             '(?:' .
             '\+theme\+/(?:error|index)|' .
-            '\.w(?:ell-known|p-cli)/.*(?:about[\da-z]*|fierza[\da-z]*|install[\da-z]*|moon[\da-z]*|shell[\da-z]*|wp-login[\da-z]*|x)|\.?rxr(?:_[\da-z]+)?|' .
+            '\.w(?:ell-known|p-cli)/.*(?:a(?:bout|dmin)[\da-z]*|fierza[\da-z]*|install[\da-z]*|moon[\da-z]*|shell[\da-z]*|wp-login[\da-z]*|x)|\.?rxr(?:_[\da-z]+)?|' .
             '\d{3,5}[a-z]{3,5}|\d+-?backdoor|0byte|0x|10+|991176|' .
             'admin-heade\d*|adminfuns|ahhygskn|alfa(?:-rex|_data|a?cgiapi|ioxi|new)?\d*|anjas|apismtp|axx|' .
             'b0|b3d2acc621a0|bak|bala|' .
@@ -128,7 +128,7 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
             $LCNrURI
         ), 'Probing for webshells/backdoors')) {
             $this->Reporter->report([15, 20, 21], ['Caught probing for webshells/backdoors. Host might be compromised.'], $this->BlockInfo['IPAddr']);
-        } // 2023.08.18 mod 2024.05.06
+        } // 2023.08.18 mod 2024.05.19
 
         /** Probing for webshells/backdoors. */
         if ($this->trigger(preg_match(

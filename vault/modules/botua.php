48f3dbf18a2e3f2b430f892432ade6646e6f88d2
Caleb Mazalevskis <maikuolan@gmail.com>
2025-08-02 23:20:05 +0800

    Fix false positive.

--- vault/modules/botua.php
+++ vault/modules/botua.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Bot user agents module (last modified: 2025.07.27).
+ * This file: Bot user agents module (last modified: 2025.08.02).
  *
  * False positive risk (an approximate, rough estimate only): « [ ]Low [x]Medium [ ]High »
  */
@@ -37,12 +37,12 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
     $this->trigger(preg_match('/\\((?:["\']{2})?\\)/', $UANoSpace), 'UA command injection'); // 2017.01.02
 
     $this->trigger(preg_match(
-        '/(?:_once|able|as(c|hes|sert)|c(hr|ode|ontents)|e(cho|regi|scape|val)|ex' .
-        '(ec|ists)?|f(ile|late|unction)|get(c|csv|ss?)?|i(f|nclude)|len(gth)?|ope' .
-        'n|p(ress|lace|lode|uts)|print(f|_r)?|re(ad|place|quire|store)|rot13|s(ta' .
-        'rt|ystem)|w(hil|rit)e)["\':(\[{<$]/',
+        '/(?:_once|(?<!st)able|asc|assert|c(?:hr|ode|ontents)|e(?:cho|regi|scape|' .
+        'val)|ex(?:ec|ists)?|f(?:ile|late|unction)|get(?:c|csv|ss?)?|if|include|l' .
+        'en(?:gth)?|open|p(?:ress|rint(?:f|_r)?|lace|lode|uts)|re(?:ad|place|quir' .
+        'e|store)|rot13|start|system|w(?:hil|rit)e)["\':(\[{<$]/',
         $UANoSpace
-    ), 'UA command injection'); // 2017.01.20
+    ), 'UA command injection'); // 2017.01.20 mod 2025.08.02
 
     $this->trigger(preg_match(
         '/\$(?:globals|_(cookie|env|files|get|post|request|se(rver|ssion)))/',

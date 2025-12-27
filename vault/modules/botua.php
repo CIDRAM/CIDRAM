ab17d8567c725bd3f1a8bddc6f9f1024fd7425de
Caleb Mazalevskis <maikuolan@gmail.com>
2025-12-27 15:12:17 +0800

    Bot user agents module update.

--- vault/modules/botua.php
+++ vault/modules/botua.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Bot user agents module (last modified: 2025.12.02).
+ * This file: Bot user agents module (last modified: 2025.12.27).
  *
  * False positive risk (an approximate, rough estimate only): « [ ]Low [x]Medium [ ]High »
  */
@@ -221,7 +221,7 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
         '^(?:[aim]$|(?!linkedinbot).*http-?(?:agent|client))|-xpanse|' .
         'a(?:bonti|ccserver|cme.spider|dreview/\d|jbaxy|nthill$|nyevent-http|ppengine)|' .
         'b(?:igbozz|itsight|lackbird|logsearch|logbot|salsa)|' .
-        'c(?:astlebot|atexplorador|cleaner|k=\{\}|lickagy|liqzbot|ms-?checker|ontextad|orporama|ortex/\d|rowsnest|yberpatrol)|' .
+        'c(?:astlebot|atexplorador|cleaner|eramic|k=\{\}|lickagy|liqzbot|ms-?checker|ontextad|orporama|ortex/\d|rowsnest|yberpatrol)|' .
         'd(?:eepfield|le_spider|nbcrawler|omainappender|ummyconnection|umprendertree)|' .
         'expanse|' .
         'f(?:lightdeckreportsbot|luid/|orms\.gle)|' .
@@ -235,7 +235,7 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
         'p(?:4bot|4load|acrawler|ageglimpse|aloalto(?:company|network)|andalytics|arsijoo|egasusmonitoring|hantomjs|hpcrawl|ingdom|rlog|ython-httpx)|' .
         'r(?:arelyused|obo(?:cop|spider)|yze)|' .
         's(?:/got|can\.lol|can(?:ner|info)|creener|eekport|itedomain|mut|nap(?:preview)?bot|oapclient|ocial(?:ayer|searcher)|oso|pyglass|quider|treetbot|ynapse)|' .
-        't(?:omba|weezler|ryghost)|' .
+        't(?:erracotta|omba|weezler|ryghost)|' .
         'urlappendbot|urltest|' .
         'vicibox|' .
         'w(?:asalive|atchmouse|eb(?:-monitoring|bot|masteraid|money|pros|site-info\.net|thumbnail)|hatweb|ikiapiary|ininet|maid\.com|pbot/1\.|sr-agent|wwtype)|' .
@@ -245,7 +245,7 @@ $this->CIDRAM['ModuleResCache'][$Module] = function () {
     ) || preg_match(
         '~^Mozilla/5\.0( [A-Za-z]{2,5}/0\..)?$~',
         $this->BlockInfo['UA']
-    ), 'Unauthorised'); // 2023.09.15 mod 2025.11.06
+    ), 'Unauthorised'); // 2023.09.15 mod 2025.12.27
 
     if ($this->trigger(preg_match('~ivre-|masscan~', $UANoSpace), 'Port scanner and synflood tool detected')) {
         $this->Reporter->report([14, 15, 19], ['MASSCAN port scanner and synflood tool detected.'], $this->BlockInfo['IPAddr']);

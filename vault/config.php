a00ab0d40f70e454dd26b5fc1dde0b98a01d533d
Caleb Mazalevskis <maikuolan@gmail.com>
2026-02-14 23:50:34 +0800

    Fix false positive (#656).
    Changelog excerpt:
    - Replaced UptimeRobot's default signature bypass with full verification
      support.

--- vault/config.php
+++ vault/config.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Configuration handler (last modified: 2025.11.03).
+ * This file: Configuration handler (last modified: 2026.02.13).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -17,7 +17,7 @@ if (!defined('CIDRAM')) {
 }
 
 /** CIDRAM version number (SemVer). */
-$CIDRAM['ScriptVersion'] = '2.17.2';
+$CIDRAM['ScriptVersion'] = '2.17.3';
 
 /** CIDRAM version identifier (complete notation). */
 $CIDRAM['ScriptIdent'] = 'CIDRAM v' . $CIDRAM['ScriptVersion'];
